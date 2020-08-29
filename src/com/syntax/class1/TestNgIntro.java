dcd7e4af7e54ac71f89e9144150f8772fee7ebb2
DStone126 <dmstone0219@gmail.com>
2020-08-29 11:36:57 -0400

    TestNg Intro Class

---
+++ src/com/syntax/class1/TestNgIntro.java
@@ -0,0 +1,26 @@
+package com.syntax.class1;
+
+import org.testng.annotations.Test;
+
+public class TestNgIntro {
+
+	@Test
+	public void testOne() {
+		System.out.println("I'm test 1");
+	}
+	
+	@Test
+	public void testTwo() {
+		System.out.println("I'm test 2");
+	}
+	
+	@Test
+	public void testThree() {
+		System.out.println("I'm test 3");
+	}
+	
+	@Test
+	public void testFour() {
+		System.out.println("I'm test 4");
+	}
+}
---
+++ src/com/syntax/class1/TestNgTest.java
@@ -0,0 +1,14 @@
+package com.syntax.class1;
+
+import org.testng.annotations.Test;
+
+public class TestNgTest {
+ 
+	@Test 
+public void hello() {
+	System.out.println("hello");
+	
+	
+	
+}
+}
